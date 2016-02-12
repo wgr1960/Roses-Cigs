45c0fd2ba85ab0c4e0568dd72a972459b722c3de
Bryan Cuevas <bryancuevas4@gmail.com>
2016-02-12 01:57:44 -0800

    Added second slide and some code cleanup. REMOVE ANY UNUSED CODE!!!!!! Website is starting to slow down.

--- index.php
+++ index.php
@@ -5,8 +5,25 @@
 				  <ul class="slides-container">
 				    <li id="slide-1">
 				    </li>
+
 				    <li id="slide-2"> 
+				    	<div id="music-content">
+							<div class="album-cover">
+								<img id="album-img" src="wp-content/themes/roses-theme/library/images/album_cover.jpg" alt="Roses & Cigarettes - Self-Titled Album Cover"/>
+							</div>
+							<div class="album-info">
+								<p>The self-titled debut Roses & Cigarettes is out now.</p>
+								<p>Featuring "Whiskey Down", "Gypsy Woman",</p>
+								<p>& "Laurel Canyon"</p>
+								<div id='album-buttons'>
+									<img id='spotify' src='wp-content/themes/roses-theme/library/images/listen-on-spotify.png' />
+									<img id='itunes' src='wp-content/themes/roses-theme/library/images/itunes_logo.png' />
+								</div>
+							</div>
+						</div>
 				    </li>
+
+
 				    <li id="slide-3">
 				    </li>
 				  </ul>
@@ -46,18 +63,6 @@
 									<section class="entry-content cf">
 										<?php the_content(); ?>
 									</section>
-
-									<!-- <footer class="article-footer cf">
-										<p class="footer-comment-count">
-											<?cphp /*comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );*/?>
-										</p> -->
-
-
-	                 	<?php /*printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') );*/ ?>
-
-	                  <?php /*the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' );*/ ?>
-
-
 									</footer>
 
 								</article>
@@ -151,25 +156,6 @@
 				</div>
 			</div>
 
-			<div id="music">
-				<div id="music-content">
-					<div class="header-container">
-						<h1>Music</h1>
-					</div>
-					<div class="album-cover">
-						<img id="album-img" src="wp-content/themes/Roses-Cigs-master/library/images/album_cover.jpg" alt="Roses & Cigarettes - Self-Titled Album Cover"/>
-					</div>
-					<div class="album-info">
-						<p>The self-titled debut Roses & Cigarettes is out now.</p>
-						<p>Featuring "Whiskey Down", "Gypsy Woman",</p>
-						<p>& "Laurel Canyon"</p>
-						<button id="roses-button">Buy Now</button>
-					</div>
-
-<!-- 					<iframe id="spotify_player" alt="Roses & Cigarettes - Spotify Player" src="https://embed.spotify.com/?uri=spotify:artist:2JZd7083SkKl8xV7zS2xIS" width="300" height="380" frameborder="0" allowtransparency="true"></iframe> -->
-<!-- 				</div>
- -->			</div>
- 			</div>
 			<div id="contact">
 			</div>
 
