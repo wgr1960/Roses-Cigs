8a692e80aa7bfb33cdfbaf6919a284a8564ec67a
Bryan Cuevas <bryancuevas4@gmail.com>
2016-02-15 19:42:46 -0800

    Final fixes to footer and nav bar for mobile

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 				<div id="inner-footer">
-					<div class='copyright-container'>
+					<div class="footer-container copyright-container">
 						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
 					</div>
-					<div class="social-container">
+					<div class="footer-container social-container">
 						<div class="social">
+							<p>
 				            <a href="https://instagram.com/rosesandcigarettesband/" target="blank"><i class="fa fa-instagram fa-lg"></i></a>
 				            <a href="https://twitter.com/rosescigarette" target="blank"><i class="fa fa-twitter fa-lg"></i></a>
 				            <a href="https://www.facebook.com/rosesandcigarettesband" target="blank"><i class="fa fa-facebook fa-lg"></i></a>
@@ -12,6 +13,7 @@
 				            <a href="https://soundcloud.com/roses-and-cigarettes"><i class="fa fa-soundcloud" target="blank"></i></a>
 				            <a href="spotify:artist:2JZd7083SkKl8xV7zS2xIS"><i class="fa fa-spotify fa-lg" target="blank"></i></a>
 				            <a href="https://itunes.apple.com/us/album/roses-and-cigarettes/id987734711" target="blank"><i class="fa fa-apple fa-lg"></i></a>
+				            </p>
 		            	</div>
 	            	</div>
 				</div>
--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow" class='desktop-logo'><?php bloginfo('name'); ?></a><a href="<?php echo home_url(); ?>" rel="nofollow" class='mobile-logo'>R & C</a></p>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
