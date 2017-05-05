74248a043279367d5dea45be96ce1fca6b19e76a
Bryan Cuevas <bryancuevas@Bryans-MacBook-Pro.local>
2017-05-05 00:49:32 -0700

    Added mailchimp newsletter form

--- index.php
+++ index.php
@@ -94,6 +94,49 @@
 	<?php masterslider(2); ?>
 </section>
 
+<!-- Newsletter -->
+<section id="newsletter">
+	<div class="columns">
+		<div class="column is-centered">
+			<!-- Begin MailChimp Signup Form -->
+			<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+			<style type="text/css">
+				#mc_embed_signup{ 
+					color: darkgrey;
+					background: linear-gradient(rgba(32, 29, 22, 0.95), rgba(32, 29, 22, 0.95));
+    				padding: 35px 0;
+					font-family: RosesRustic;
+				}
+			</style>
+			<div id="mc_embed_signup">
+				<form action="//rosesandcigarettes.us10.list-manage.com/subscribe/post?u=a784f2e3f97f791038f8893df&amp;id=613a2e44f7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    		<div id="mc_embed_signup_scroll">
+			<h2 class="has-text-centered">Subscribe to our Newsletter!</h2>
+			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div class="mc-field-group">
+				<label for="mce-FNAME">First Name </label>
+				<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+			</div>
+			<div class="mc-field-group">
+				<label for="mce-LNAME">Last Name </label>
+				<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a784f2e3f97f791038f8893df_613a2e44f7" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div></div></form></div>
+
+<!--End mc_embed_signup-->
+		</div>
+	</div>
+</section>
+
 
 <!-- Contact -->
 <section id="contact" class="even-section">
