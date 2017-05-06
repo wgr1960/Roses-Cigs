a093a912b9ac920cffb11c8d25e096b0206c681e
Bryan Cuevas <bryancuevas@Bryans-MacBook-Pro.local>
2017-05-06 00:28:38 -0700

    Re-did the songkick api query

--- js/custom.js
+++ js/custom.js
@@ -16,28 +16,22 @@ function formatDate(unformattedDate) {
 		pageLinks[i].href = oldLink + "#primary"; 
 	}
 
-	fetch("http://api.songkick.com/api/3.0/artists/8552809/calendar.json?apikey=nb73Yx2o9AEEmBh7", {method: "GET"})
-		.then(function(resp) {
-			return resp.json();
-		})
-		.then(function(data) {
-			var upcomingEvents = data;
-			var eventCount = upcomingEvents.resultsPage.results.event.length;
-			upcomingHTMLTable += "<tbody>";
-			for(var i = 0; i < eventCount; i++) {
-				var venueLocation = upcomingEvents.resultsPage.results.event[i].location.city;
-				var venueName 	  = upcomingEvents.resultsPage.results.event[i].venue.displayName;
-				var venueTickets  = upcomingEvents.resultsPage.results.event[i].uri;
-				var unformattedDate = upcomingEvents.resultsPage.results.event[i].start.date;
-				var formattedDate 	= formatDate(unformattedDate);
+	var url = "http://api.songkick.com/api/3.0/artists/8552809/calendar.json?apikey=nb73Yx2o9AEEmBh7&jsoncallback=?";
+	jQuery.getJSON(url, function(data) {
+		var upcomingEvents = data;
+		var eventCount = upcomingEvents.resultsPage.results.event.length;
+		upcomingHTMLTable += "<tbody>";
+		for(var i = 0; i < eventCount; i++) {
+			var venueLocation = upcomingEvents.resultsPage.results.event[i].location.city;
+			var venueName 	  = upcomingEvents.resultsPage.results.event[i].venue.displayName;
+			var venueTickets  = upcomingEvents.resultsPage.results.event[i].uri;
+			var unformattedDate = upcomingEvents.resultsPage.results.event[i].start.date;
+			var formattedDate 	= formatDate(unformattedDate);
 
-				upcomingHTMLTable += "<tr><td>" + formattedDate + "</td><td>" + venueName + "</td><td>" + venueLocation + "</td><td><a href='" + venueTickets + "' target='_blank'>Tickets</a></td>"; 
-			}
-			upcomingHTMLTable += "</tbody></div>";
-			var targetDiv = document.getElementById('gigs').getElementsByClassName("container")[0];
-			targetDiv.innerHTML = upcomingHTMLTable;
-		})
-		.catch(function(err) {
-
-		});
+			upcomingHTMLTable += "<tr><td>" + formattedDate + "</td><td>" + venueName + "</td><td>" + venueLocation + "</td><td><a href='" + venueTickets + "' target='_blank'>Tickets</a></td>"; 
+		}
+		upcomingHTMLTable += "</tbody></div>";
+		var targetDiv = document.getElementById('gigs').getElementsByClassName("container")[0];
+		targetDiv.innerHTML = upcomingHTMLTable;
+	});
 })();
